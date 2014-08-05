b060536b8db7881897b329bb439e9cf857af63f9
Drew <drew.ditthardt@gmail.com>
2014-08-04 23:23:32 -0700

    implemented output

--- dorf/start.lua
+++ dorf/start.lua
@@ -2,13 +2,46 @@ local lanes = require 'lanes'.configure()
 local l = lanes.linda()
 local config = require 'config'
 local m = lanes.require 'message'
+local uuid = require 'uuid'
 
 local inputs, outputs = {}, {}
+
 for _, v in pairs(config.inputs) do
   local message = m(l, 'game')
-  inputs[#inputs+1] = lanes.gen('*', function()
-    require(v)(message)
+  inputs[uuid.new()] = lanes.gen('*', function()
+    local ok, err = pcall(function() require(v)(message) end)
+    if not ok then print(err) end
   end)()
 end
+
+for _, v in pairs(config.outputs) do
+  local id = uuid.new()
+  outputs[id] = lanes.gen('*', function()
+    local message = m(id)
+    local data
+    repeat
+      data = message.receive()
+      local ok, err = pcall(function() require(v)(data[1], data[2]) end)
+      if not ok then print(err) end
+    until not data
+  end)()
+end
+
+local outputMessage = {}
+
+for k, v in pairs(outputs) do
+  outputMessage[#outputMessage+1] = m(k)
+end
+
+lanes.gen(function()
+  local message = m(l, 'output')
+  local data
+  repeat
+    data = message.receive()
+    for _, v in pairs(outputMessage) do
+      v.send(data)
+    end
+  until not data
+end)
 
 l:receive('exit')
