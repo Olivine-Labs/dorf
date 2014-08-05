90c0229cf5154890c0ebb349f0c0be9c5ac7f49a
Jack Lawson <ajacksified@gmail.com>
2014-08-04 23:01:29 -0700

    No really, fix terminal

--- dorf/input/terminal.lua
+++ dorf/input/terminal.lua
@@ -2,7 +2,7 @@ local stringx = require 'pl.stringx'
 
 local function parse(command)
   local success
-  local command, data = stringx.split(command, ' ', 2)
+  local command, data = unpack(stringx.split(command, ' ', 2))
 
   success, command, data = pcall(function()
     return require('input.terminal.' .. command)(data)
@@ -18,23 +18,20 @@ local function parse(command)
 end
 
 return function(msg)
-  local ok, err = pcall(function()
-    local command, data
+  local command, data
 
-    repeat
-      command = io.read()
+  repeat
+    command = io.read()
 
-      if command and command ~= 'exit' then
-        command, data = parse(command)
+    if command and command ~= 'exit' then
+      command, data = parse(command)
 
-        if command then
-          msg.send(command, data)
-        end
+      if command then
+        msg.send(command, data)
       end
-    until command == 'exit'
+    end
+  until command == 'exit'
 
-    msg.send('exit')
-  end)
+  msg.send('exit')
 
-  if not ok then print(err) end
 end
--- dorf/input/terminal/move.lua
+++ dorf/input/terminal/move.lua
@@ -1,8 +1,7 @@
-local stringx = 'pl.stringx'
+local stringx = require 'pl.stringx'
 
--- "10 1,2"
 return function(data)
-  local id, coords = stringx.split(data, ' ', 2)
-  local x,y = stringx.split(coords, ',', 2)
+  local id, coords = unpack(stringx.split(data, ' ', 2))
+  local x,y = unpack(stringx.split(coords, ',', 2))
   return 'move', { id = id, x = x, y = y }
 end
