81bc64b428bf194edf1b83087347db379f84f1c2
Drew <drew.ditthardt@gmail.com>
2014-08-06 11:29:30 -0700

    stubbed movement using world share

--- dorf/game.lua
+++ dorf/game.lua
@@ -3,10 +3,31 @@ return function(input, output)
   local g = {}
   local world = share()
 
+  world.entities = {}
+  world.entities['1'] = {
+    speed = 10,
+    x=1,
+    y=1,
+    z=1
+  }
   function g.input()
     local cmd, data = input.receive(0)
     if cmd then
-      data.speed = 10
+      print(data.id)
+      local entity = world.entities[data.id]
+      if data.x then
+        entity.x = entity.x+data.x
+      end
+      if data.y then
+        entity.y = entity.y+data.y
+      end
+      if data.z then
+        entity.z = entity.z+data.z
+      end
+      data.speed = entity.speed
+      data.x = entity.x
+      data.y = entity.y
+      data.z = entity.z
       output.send(cmd, data)
     end
   end
